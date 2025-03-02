0773202cbbcdd25a8db4308d0ac79d6593f6ce38
lukemader <lukemader@protonmail.com>
2025-03-02 15:49:49 +0000

    Changed loadMarkDownContent.js to use fetch but include the window.onload function

--- loadMarkDownContent.js
+++ loadMarkDownContent.js
@@ -1,54 +1,25 @@
-// Async function to load and display markdown content
-// async function loadMarkdownContent(markdownFile, elementId) {
-//     try {
-//         const repo = "https://raw.githubusercontent.com/lukemader/lukemader.github.io/master/";
-//         const file = repo + markdownFile; 
-//         const response = await fetch(file);
-
-//         if (!response.ok) {
-//             throw new Error(`Failed to load ${file}. Status: ${response.status}`);
-//         }
-
-//         const markdown = await response.text();
-//         const converter = new showdown.Converter();
-//         const htmlContent = converter.makeHtml(markdown);
-//         document.getElementById(elementId).innerHTML = htmlContent;
-//     } catch (error) {
-//         console.error('Error loading markdown file:', error);
-//         document.getElementById(elementId).innerHTML = `Error: ${error.message}`; 
-//     }
-// }
-
-// function loadMarkdownContent(markdownFile, elementID){
-//     reader = new FileReader();
-//     reader.readAsText(markdownFile);
-//     const markdown = reader.
-// }
-
-function loadMarkdownContent(markdownFile, elementId) {
+async function loadMarkdownContent(markdownFile, elementId) {
     try {
-        const reader = new FileReader();
+        const repo = "https://raw.githubusercontent.com/lukemader/lukemader.github.io/master/";
+        const file = repo + markdownFile;
+        const response = await fetch(file);
 
-        reader.onload = function(event) {
-            const markdown = event.target.result;
-            const converter = new showdown.Converter();
-            const htmlContent = converter.makeHtml(markdown);
-            document.getElementById(elementId).innerHTML = htmlContent;
-        };
+        if (!response.ok) {
+            throw new Error(`Failed to load ${file}. Status: ${response.status}`);
+        }
 
-        reader.readAsText(markdownFile);
+        const markdown = await response.text();
+        const converter = new showdown.Converter();
+        const htmlContent = converter.makeHtml(markdown);
+        document.getElementById(elementId).innerHTML = htmlContent;
     } catch (error) {
         console.error('Error loading markdown file:', error);
         document.getElementById(elementId).innerHTML = `Error: ${error.message}`;
     }
 }
 
-
-// Load content for the Teaching section
-loadMarkdownContent('assets/markdown/teaching.md', 'teaching-markdown-content');
-
-// Load content for the Education section
-loadMarkdownContent('assets/markdown/education.md', 'education-markdown-content');
-
-// Load content for the Talks & Seminars section
-loadMarkdownContent('assets/markdown/talks.md', 'talks-markdown-content');
+window.onload = function() {
+    loadMarkdownContent("talks.md", "talks-markdown-content");
+    loadMarkdownContent("education.md", "education-markdown-content");
+    loadMarkdownContent("teaching.md", "teaching-markdown-content");
+};
---
+++ talks.md
@@ -0,0 +1,37 @@
+# Upcoming - Atoms, lasers, and making sense of the rotating wave approximation
+
+**Seminar:** [Lancaster University Postgraduate Forum](https://www.lancaster.ac.uk/maths/)
+
+**Location:** Lancaster University, United Kingdom
+
+**Dates:** 25/03/2025
+
+**Abstract:** TBC
+
+**Slides:** TBC
+
+# An Introduction to Quantum Computers and Hidden Subgroup Problems
+
+**Reading Group:** [Post-Quantum Cryptography Reading Group](https://www.lancaster.ac.uk/maths/)
+
+**Location:** Lancaster University, United Kingdom
+
+**Dates:** 18/09/2024, 23/09/2024
+
+**Slides:** [Download](assets/slides/intro_to_quantum_algorithms_both_talks.pdf)
+
+# An Invitation to Mathematical Quantum Physics
+
+**Seminar:** [Lancaster University Postgraduate Forum](https://www.lancaster.ac.uk/maths/)
+
+**Location:** Lancaster University, United Kingdom
+
+**Date:** 27/11/2023
+
+**Abstract:** In the early 20th century, experiments showed that quantum particles behave very differently compared to the non-quantum world that we are used to. Not only do quantum particles exhibit both wave-like and particle-like behaviour, but they also have a probabilistic nature to them. The demand on mathematicians was that we needed new theory suitable to tackle these phenomena.
+
+ 
+
+In this talk, we place ourselves in the shoes of a mathematician at the time who wishes to rigorously develop this theory. We look at how quantum physics has a natural framework through unbounded operators on Hilbert spaces, and some key differences between unbounded operators and bounded operators. We then turn to applying this theory in a quantum physics context.
+
+**Slides:** [Download](assets/slides/invitation_to_mathematical_quantum_physics.pdf)
